aac9987674ec8f0d10005795641a32a4def3c4ea
Mekala Sai Kiran Reddy <62593093+SaiKiranReddy28@users.noreply.github.com>
2022-11-06 21:43:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask,request,jsonify,render_template
 import pickle
-
+import taxi_price_model
 app=Flask(__name__)
 model=pickle.load(open("model.sav","rb"))
 
@@ -13,14 +13,19 @@ def home():
 def predict():
 
     int_features=[float(x) for x in request.form.values()]
+    val=taxi_price_model.haversine(int_features[0],int_features[1],int_features[2],int_features[3])
+    
+    int_features.append(val)
     final_features=[np.array(int_features)]
     prediction=model.predict(final_features)
-
+    
     output=round(prediction[0],2)
 
-    return render_template("index.html",prediction_text="The price for the trip is ${}".format(output))
+    return render_template("index.html",prediction_text="The price for the trip is ${}".format(output),distance="The approx distance of travel is {}".format(val))
 
 
 if __name__=="__main__":
     app.run(debug=True)
+    
+    
 
